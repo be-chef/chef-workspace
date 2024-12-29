15cf5d361c413666d141a4d42a813cf218bc82c3
PS8924 <shah.pankti@northeastern.edu>
2024-12-29 13:35:59 -0800

    feat: Setup react component catalog - Slushy

---
+++ slushy/src/lib/slushy.tsx
@@ -0,0 +1,11 @@
+import styles from './slushy.module.scss';
+
+export function Slushy() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Slushy!</h1>
+    </div>
+  );
+}
+
+export default Slushy;

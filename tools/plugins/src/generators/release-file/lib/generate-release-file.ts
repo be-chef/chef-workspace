ac6b9669a3f1da0e36e7efbde715b470122576aa
PS8924 <shah.pankti@northeastern.edu>
2025-01-04 20:33:34 -0800

    fix: release-file object shape has changed

--- tools/plugins/src/generators/release-file/lib/generate-release-file.ts
+++ tools/plugins/src/generators/release-file/lib/generate-release-file.ts
@@ -3,7 +3,12 @@ import {ReleaseFileOptions} from '../schema';
 import { writeFileSync } from 'node:fs';
 
 export async function generateReleaseFile(tree:Tree, schema:ReleaseFileOptions ) {
+  
   if(tree.exists("release-file.json"))
     throw new Error("release-file.json already exists! If you want to proceed with current package deployment, remove the existing file & run npm run generate-release-file");
-  writeFileSync("release-file.json", JSON.stringify({"projectName":schema.projectName, "bumpType":schema.bumpType}));
+    const versionDetail = {
+      "projectName":schema.projectName, 
+      "bumpType":schema.bumpType
+    }
+  writeFileSync("release-file.json", JSON.stringify(versionDetail));
 }
